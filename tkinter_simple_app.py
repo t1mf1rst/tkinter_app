03411ee1477aad68e09094c9536f8070aab8bf41
Tim First <timfirst0990@gmail.com>
2018-07-09 21:48:45 +0600

    intro to tkinter

---
+++ tkinter_simple_app.py
@@ -0,0 +1,21 @@
+# library for creating gui apps
+from tkinter import *
+
+# create window: all content of window should be between window and window.mainloop()
+window = Tk()
+
+def km_to_miles():
+	miles = float( e1_value.get() ) * 1.6
+	t1.insert( END, miles )
+
+b1 = Button(window, text="execute", command=km_to_miles)
+b1.grid(row=0, column=0)
+
+e1_value=StringVar()
+e1 = Entry(window, textvariable=e1_value)
+e1.grid(row=0, column=1)
+
+t1 = Text(window, height=1, width=20)
+t1.grid(row=0, column=2)
+
+window.mainloop()
